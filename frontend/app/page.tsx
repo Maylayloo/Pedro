3efdbb91cb66434e48da3c88933f1112f3804575
Maylayloo <milosz.l-24@wp.pl>
2025-06-09 00:06:22 +0200

    deleted icons, added Pedro logo

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-    return (
-        <div className='flex-center h-screen bg-violet-400'>
-            <img src="favicon.ico" alt="pedro_logo" width={64}/>
-            <h1 className="text-4xl underline">"The future begins today."</h1>
-        </div>
-    );
-}
