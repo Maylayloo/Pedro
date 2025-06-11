5452b97572c7ffb319c48d4bc2d47a6e39a5e5ff
NotBadKid <dawidm13@interia.pl>
2025-06-12 00:52:41 +0200

    debugging User repository

--- backend/src/main/java/com/example/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/user/repository/UserRepository.java
@@ -1,16 +1,14 @@
 package com.example.backend.user.repository;
 
-import com.example.backend.user.model.User;
+import com.example.backend.user.model.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
-    List<User> findByUsernameContainingIgnoreCase(String username);
-    List<User> findByEmailContainingIgnoreCase(String email);
-    List<User> findByFirstNameContainingIgnoreCase(String firstName);
-    List<User> findByLastNameContainingIgnoreCase(String lastName);
+public interface UserRepository extends JpaRepository<MyUser, Long> {
+    Optional<MyUser> findByEmail(String email);
+    Optional<MyUser> findByNickname(String nickname);
+    List<MyUser> findByNicknameContainingIgnoreCase(String username);
+    List<MyUser> findByEmailContainingIgnoreCase(String email);
 }
