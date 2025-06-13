1fcef81d804b9d772d5ac363a474e52d5982cb11
NotBadKid <dawidm13@interia.pl>
2025-06-13 14:23:35 +0200

    renamed UserRepository to MyUserRepository

--- backend/src/main/java/com/example/backend/user/repository/MyUserRepository.java
+++ backend/src/main/java/com/example/backend/user/repository/MyUserRepository.java
@@ -6,9 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<MyUser, Long> {
+public interface MyUserRepository extends JpaRepository<MyUser, Long> {
     Optional<MyUser> findByEmail(String email);
     Optional<MyUser> findByNickname(String nickname);
-    List<MyUser> findByNicknameContainingIgnoreCase(String username);
-    List<MyUser> findByEmailContainingIgnoreCase(String email);
 }
