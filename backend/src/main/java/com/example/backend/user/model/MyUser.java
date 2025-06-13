fe63e4668c5181e449b15ec4b0981ebefdc2a011
NotBadKid <dawidm13@interia.pl>
2025-06-13 14:24:12 +0200

    renamed get/setUserId to just get/setId

--- backend/src/main/java/com/example/backend/user/model/MyUser.java
+++ backend/src/main/java/com/example/backend/user/model/MyUser.java
@@ -84,11 +84,11 @@ public class MyUser {
         this.status = status;
     }
 
-    public Long getUserId() {
+    public Long getId() {
         return userId;
     }
 
-    public void setUserId(Long userId) {
+    public void setId(Long userId) {
         this.userId = userId;
     }
 }
