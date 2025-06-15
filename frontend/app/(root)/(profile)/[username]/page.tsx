6f2bc68ca8b1d21e0020465921f8ee10659633c1
Maylayloo <milosz.l-24@wp.pl>
2025-06-15 15:55:52 +0200

    apparently params are asynchronous rn

--- frontend/app/(root)/(profile)/[username]/page.tsx
+++ frontend/app/(root)/(profile)/[username]/page.tsx
@@ -1,22 +1,21 @@
+'use client'
+
 import OfflineProfile from "@/app/(root)/(profile)/components/offlineProfile";
 import OnlineProfile from "@/app/(root)/(profile)/components/onlineProfile";
 
 
-interface props {
-    params: {
-        username: string;
-    }
-}
+const UserPage = async ({params}: { params: Promise<{ username: string }> }) => {
 
-const UserPage = ({params}: props) => {
+    const { username } = await params
+
 
     // for now
     const status = "online";
 
 
     return status === "online" ?
-        <OnlineProfile username={params.username}/>
-        : <OfflineProfile username={params.username}/>
+        <OnlineProfile username={username}/>
+        : <OfflineProfile username={username}/>
 };
 
 export default UserPage;
