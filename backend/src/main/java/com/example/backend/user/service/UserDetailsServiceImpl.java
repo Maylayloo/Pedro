ef36a4d010d3e7b64f65ab79663b4d7856dd7394
NotBadKid <dawidm13@interia.pl>
2025-06-18 01:27:25 +0200

    changed name of CustomUserDetailsService

--- backend/src/main/java/com/example/backend/user/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/example/backend/user/service/UserDetailsServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private MyUserRepository userRepository;
