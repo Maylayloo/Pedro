a0c7fad6305b35512a21a3286cc56c1d6a9ff4d2
NotBadKid <dawidm13@interia.pl>
2025-06-29 13:38:17 +0200

    updating get message service

--- backend/src/main/java/com/example/backend/chat/service/ChatMessageService.java
+++ backend/src/main/java/com/example/backend/chat/service/ChatMessageService.java
@@ -1,13 +1,20 @@
 package com.example.backend.chat.service;
 
-import com.example.backend.chat.dto.ChatMessageDTO;
+import com.example.backend.chat.dto.ChatMessageRequestDTO;
+import com.example.backend.chat.dto.ChatMessageResponseDTO;
 import com.example.backend.chat.model.ChatMessage;
 import com.example.backend.chat.repository.ChatMessageRepository;
-import jakarta.persistence.Access;
+import com.example.backend.stream.model.Stream;
+import com.example.backend.stream.repo.StreamRepo;
+import com.example.backend.user.model.MyUser;
+import com.example.backend.user.repository.MyUserRepository;
+import com.example.backend.user.service.UserDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,13 +23,27 @@ public class ChatMessageService {
     @Autowired
     private ChatMessageRepository chatMessageRepository;
 
-    public void saveMessage(ChatMessageDTO messageDTO){
-        ChatMessage chatMessage = new ChatMessage(messageDTO);
-        chatMessageRepository.save(chatMessage);
+    @Autowired
+    private UserDataService myUserService;
+
+    @Autowired
+    private StreamRepo streamRepo;
+
+    public void saveMessage(ChatMessageRequestDTO requestDTO, Long streamId) {
+        Optional<MyUser> user = myUserService.getLoggedInUser();
+
+        Stream stream = streamRepo.findById(streamId)
+                .orElseThrow(() -> new RuntimeException("Stream not found"));
+        ChatMessage message = new ChatMessage(
+                user.get(),
+                requestDTO.getMessage(),
+                stream
+        );
+        chatMessageRepository.save(message);
     }
 
-    public List<ChatMessageDTO> getMessagesForStream(Long streamId){
-        return chatMessageRepository.findByStreamId(streamId).stream().
-                map(ChatMessageDTO::new).collect(Collectors.toList());
+    public List<ChatMessageResponseDTO> getMessagesForStream(Long streamId){
+        return chatMessageRepository.findTop50ByStreamIdOrderByTimestampDesc(streamId).stream().
+                map(ChatMessageResponseDTO::new).collect(Collectors.toList());
     }
 }
