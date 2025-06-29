01a4b2d1e1fa8455cf4e9438705ea2025b109af5
NotBadKid <dawidm13@interia.pl>
2025-06-29 13:39:10 +0200

    implemented send message endpoint

--- backend/src/main/java/com/example/backend/chat/controller/ChatWebSocketController.java
+++ backend/src/main/java/com/example/backend/chat/controller/ChatWebSocketController.java
@@ -1,14 +1,20 @@
 package com.example.backend.chat.controller;
 
-import com.example.backend.chat.model.ChatMessage;
+import com.example.backend.chat.dto.ChatMessageRequestDTO;
+import com.example.backend.chat.dto.ChatMessageResponseDTO;
 import com.example.backend.chat.service.ChatMessageService;
+import com.example.backend.user.model.MyUser;
+import com.example.backend.user.repository.MyUserRepository;
+import com.example.backend.user.service.UserDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Controller
 public class ChatWebSocketController {
@@ -19,9 +25,24 @@ public class ChatWebSocketController {
     @Autowired
     private ChatMessageService chatMessageService;
 
+    @Autowired
+    private UserDataService userDataService;
+
     @MessageMapping("/chat/{streamId}")
-    public void sendMessage(@DestinationVariable Long streamId, ChatMessage message) {
-        chatMessageService.saveMessage()
+    @SendTo("/topic/chat/{streamId}")
+    public void sendMessage(@DestinationVariable Long streamId, ChatMessageRequestDTO messageDTO) {
+
+        chatMessageService.saveMessage(messageDTO, streamId);
+
+        Optional<MyUser> userOpt = userDataService.getLoggedInUser();
+
+        ChatMessageResponseDTO responseDTO = new ChatMessageResponseDTO(
+                streamId,
+                userOpt.map(MyUser::getId).orElse(null),
+                messageDTO.getMessage(),
+                userOpt.get().getNickname(),
+                LocalDateTime.now()
+        );
     }
 
 }
