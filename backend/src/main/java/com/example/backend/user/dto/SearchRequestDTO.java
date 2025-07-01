13ae9c4f4af4e103a56b81b5cabd75999588f9a2
NotBadKid <dawidm13@interia.pl>
2025-07-02 00:52:56 +0200

    Implemented searching users with nicknames

---
+++ backend/src/main/java/com/example/backend/user/dto/SearchRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.backend.user.dto;
+
+public class SearchRequestDTO {
+    private String query;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
---
+++ backend/src/main/java/com/example/backend/user/service/UserSearchService.java
@@ -0,0 +1,24 @@
+package com.example.backend.user.service;
+
+import com.example.backend.user.dto.SearchResponseDTO;
+import com.example.backend.user.repository.MyUserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserSearchService {
+    private final MyUserRepository userRepository;
+
+    public UserSearchService(MyUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<SearchResponseDTO> searchUsers(String query) {
+        System.out.println("Searching for users with query: " + query);
+        return userRepository.findTop5ByNicknameIgnoreCaseContaining(query)
+                .stream()
+                .map(user -> new SearchResponseDTO(user.getId(), user.getNickname()))
+                .toList();
+    }
+}
