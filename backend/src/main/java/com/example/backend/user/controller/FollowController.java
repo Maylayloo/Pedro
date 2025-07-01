56824f8c4e6ed49fce90a45291549539813cb45a
NotBadKid <dawidm13@interia.pl>
2025-07-02 00:53:40 +0200

    Implemented getting users you follow

--- backend/src/main/java/com/example/backend/user/controller/FollowController.java
+++ backend/src/main/java/com/example/backend/user/controller/FollowController.java
@@ -1,11 +1,15 @@
 package com.example.backend.user.controller;
 
+import com.example.backend.user.dto.FollowedUserDTO;
+import com.example.backend.user.repository.FollowRepository;
 import com.example.backend.user.service.FollowService;
+import com.example.backend.user.service.UserDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,6 +18,10 @@ public class FollowController {
 
     @Autowired
     private FollowService followService;
+    @Autowired
+    private UserDataService userDataService;
+    @Autowired
+    private FollowRepository followRepository;
 
     @PostMapping("/{userId}")
     public ResponseEntity<?> followUser(@PathVariable Long userId) {
@@ -53,5 +61,15 @@ public class FollowController {
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
+    }
+
+    @GetMapping("/myFollow")
+    public ResponseEntity<List<FollowedUserDTO>> getFollowedUsers() {
+        return userDataService.getLoggedInUser()
+                .map(user -> {
+                    List<FollowedUserDTO> followed = followRepository.findFollowedUsersWithStatus(user.getId());
+                    return ResponseEntity.ok(followed);
+                })
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 }
