726be4cc244c42ca26df471d2c89c72fe67710fa
NotBadKid <dawidm13@interia.pl>
2025-07-03 16:30:30 +0200

    API for getting my coins amount

--- backend/src/main/java/com/example/backend/user/controller/UserController.java
+++ backend/src/main/java/com/example/backend/user/controller/UserController.java
@@ -113,4 +113,17 @@ public class UserController {
         }
     }
 
+    @GetMapping("/me/coins")
+    public ResponseEntity<?> getMyCoins() {
+        Optional<MyUser> currentUser = userDataService.getLoggedInUser();
+
+        if (currentUser.isPresent()) {
+            MyUser user = currentUser.get();
+            return ResponseEntity.ok(user.getPedroCoin());
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("User not authenticated");
+        }
+    }
 }
